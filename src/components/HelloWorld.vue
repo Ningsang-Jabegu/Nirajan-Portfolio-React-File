96ab337240a358650dad008c7ae9da0d6c2a1a88
Ningsang Jabegu <86409265+Ningsang-Jabegu@users.noreply.github.com>
2024-01-09 11:44:11 +0545

    First Commit for Mr. N-K Rajan

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+  <p>Aaundai Xu Hai Ma Bato Ma I'm Walking, I'm Walking in the street of Thamel. Parkhi Bas!</p>
+  <p>Comming Soon !</p>
+</template>
+
+<style scoped>
+p {
+  color: #888;
+}
+</style>
