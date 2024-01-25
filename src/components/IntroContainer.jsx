a3a7b1c3721e9154e1d03253e0ba140027a7abe8
Ningsang Jabegu <86409265+Ningsang-Jabegu@users.noreply.github.com>
2024-01-25 15:37:21 +0545

    cv file path updated

--- src/components/IntroContainer.jsx
+++ src/components/IntroContainer.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import profile_pic from "../../public/images/Homepage/profile_picture.png";
+import cv from "../../public/resources/Resume-Nirajan.pdf"
 
 export default function IntroContainer(){
     return(
@@ -7,7 +8,7 @@ export default function IntroContainer(){
             <div>
                 <h1>Welcome to My Portfolio</h1>
                 <p className="text-normal">Hi, I am Nirajan Khatiwada, A Data Science Enthusiast.</p>
-                <button><a href="/public/resources/Resume-Nirajan.pdf" target="_blank">View CV <ion-icon name="eye"></ion-icon></a></button>
+                <button><a href={cv} target="_blank">View CV <ion-icon name="eye"></ion-icon></a></button>
             </div>
             <div>
                 <img src={profile_pic} />
